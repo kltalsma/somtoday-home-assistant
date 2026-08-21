5e8d7bb9d0f35c31cec7f8e8e97f42384fc8d5c9
kltalsma <32841510+kltalsma@users.noreply.github.com>
2026-08-21 08:34:10 +0200

    Update Somtoday authentication flow

--- custom_components/somtoday/__init__.py
+++ custom_components/somtoday/__init__.py
@@ -7,7 +7,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .client import SomtodayClient
-from .const import CONF_PASSWORD, CONF_SCHOOL, CONF_USERNAME, DOMAIN, PLATFORMS
+from .const import CONF_PASSWORD, CONF_TENANT_ID, CONF_USERNAME, DOMAIN, PLATFORMS
 from .coordinator import SomtodayCoordinator
 
 SomtodayConfigEntry = ConfigEntry
@@ -16,7 +16,7 @@ SomtodayConfigEntry = ConfigEntry
 async def async_setup_entry(hass: HomeAssistant, entry: SomtodayConfigEntry) -> bool:
     """Set up Somtoday from a config entry."""
     client = SomtodayClient(
-        async_get_clientsession(hass), entry.data["school_uuid"], entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD]
+        async_get_clientsession(hass), entry.data[CONF_TENANT_ID], entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD]
     )
     coordinator = SomtodayCoordinator(hass, client)
     await coordinator.async_config_entry_first_refresh()
